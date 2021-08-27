71e9b94203ea427f98291b6be536207323d941c1
Zuhair Rayyes <rayyes95@gmail.com>
2021-08-27 22:50:59 +0200

    Completed bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,33 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"strings"
+	"unicode"
+)
+
+func hasLetter(remark string) bool {
+	for _, r := range remark {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	remark = strings.TrimSpace(remark)
+	if strings.HasSuffix(remark, "?") {
+		if strings.ToUpper(remark) == remark && hasLetter(remark) {
+			return "Calm down, I know what I'm doing!"
+		}
+		return "Sure."
+	} else {
+		if strings.ToUpper(remark) == remark && hasLetter(remark) {
+			return "Whoa, chill out!"
+		}
+		if len(remark) == 0 {
+			return "Fine. Be that way!"
+		}
+		return "Whatever."
+	}
 }
